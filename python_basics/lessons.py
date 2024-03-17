1669b2011c6dbc5ba432e0e4bb024b7aba303e35
joshua-ed-cox <jec.professions@gmail.com>
2024-03-16 22:48:44 -0600

    added basic data type lessons & changed file name

---
+++ python_basics/lessons.py
@@ -0,0 +1,98 @@
+# Augmented Assignment opperators
+# some_value = 2
+# some_value += 8
+
+# another_value = 8
+# another_value *= 2
+
+# print(some_value)
+# print(another_value)
+
+
+# long_string = '''
+# This is a multi-line string
+# it will cover multiple lines
+# very cool
+# '''
+# print(long_string)
+
+
+# Sting concatenation
+# print("Hello " + "Joshua")
+# print("Hello" + " " + "Joshua")
+
+
+# Type Conversion
+# a = 10
+# b = str(a)
+# c = type(b)
+
+# print(b)
+# print(c)
+
+
+# Escape sequences
+# \ means whatever is next is a string
+# weather = 'it\'s nice weather today'
+# how_do_you_feel = "We are feeling \"good\" today"
+# print(weather)
+# print(how_do_you_feel)
+# print()
+
+# \t adds a tab
+# weather = '\tit\'s nice weather today'
+# print(weather)
+
+# \n adds a new line
+# weather = '\nit\'s nice weather today. \nyes it is.'
+# print(weather)
+
+
+# Formatted Strings
+# name = "Joshua"
+# age = 23
+
+# print(f"Hello {name} you are {age} years old")
+# print("Hello {} you are {} years old".format(name, age))
+# print("Hello {1} you are {0} years old".format(name, age))
+# print("Hello {new_name} you are {new_age} years old".format(new_name = "candace", 
+# new_age = 100))
+
+
+# String indexes
+# string = "01234567"
+
+# [start:stop:stepover]
+# print(string[0])
+# print(string[0:])
+# print(string[:6])
+# print(string[0:8])
+# print(string[0:7:2])
+# print(string[-1])
+# print(string[::-1])
+
+
+# Immutability (Things that can not change)
+
+# string = "123456789"
+# print(string)
+# cause error must make a new string
+# string[0] = "8"
+
+# string = "8"
+# print(string)
+
+# make a new string
+# string = string + "10"
+# print(string)
+
+# Methods and built in functions
+# string = "to be or not to be"
+# string = string.replace("be", "me")
+
+# print(string)
+
+# Boolean
+# Only true or false
+# is_cool = True
+# is_cool =False
