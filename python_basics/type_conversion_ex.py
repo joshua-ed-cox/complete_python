e86937fc3fb9d276d9f54ea94b248c769684ca08
joshua-ed-cox <jec.professions@gmail.com>
2024-03-16 23:05:00 -0600

    completed the type conversion exercise

---
+++ python_basics/type_conversion_ex.py
@@ -0,0 +1,15 @@
+# Ask the user their birth year and then write a program to correctly tell the user 
+# their age.
+
+# Variable Initialization
+current_year = 2024
+
+# User inputs
+birth_year = int(input("What year were you born: "))
+
+# Calculations
+age = current_year - birth_year
+
+# Display
+print(f"You are {age} years old.")
+
