1ea42452d8927db3b0e6d4f0ad316765df1754c8
joshua-ed-cox <jec.professions@gmail.com>
2024-03-22 00:31:16 -0600

    added more lessons to lessons.py

--- python_basics/lessons.py
+++ python_basics/lessons.py
@@ -114,3 +114,21 @@
 # is_cool = True
 # is_cool =False
 # Remove after adding next lessons
+
+
+# Remove after adding next lesson
+# make a new string
+# string = string + "10"
+# print(string)
+
+# Methods and built in functions
+# string = "to be or not to be"
+# string = string.replace("be", "me")
+
+# print(string)
+
+# Boolean
+# Only true or false
+# is_cool = True
+# is_cool =False
+# Remove after adding next lessons
